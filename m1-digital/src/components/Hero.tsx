7c8e672c38248b427c9a90c3428dc66af34259e1
Ensar <ensar_mehmedovic@hotmail.com>
2026-01-11 22:18:52 +0100

    commit

--- m1-digital/src/components/Hero.tsx
+++ m1-digital/src/components/Hero.tsx
@@ -3,8 +3,8 @@ import lava from '../videos/lava.mp4'
 
 const Hero = () => {
   return (
-    <div className="relative  text-white overflow-hidden">
-        <div className='container mx-auto max-w-7xl min-h-screen flex flex-col justify-center'>
+    <div className="relative  text-primary-white min-h-screen overflow-hidden">
+        <div className='container '>
       <video
         autoPlay
         loop
@@ -12,11 +12,10 @@ const Hero = () => {
         src={lava}
         className="absolute inset-0 z-10 w-full h-full object-cover"
       />
-
-      <div className="relative z-40 flex flex-col justify-center">
+      <div className="relative z-40">
         <p>THINKING OUTSIDE THE BOX</p>
         <h1 className="text-5xl font-extrabold">DELIVERING BEYOND EXPECTATIONS.</h1>
-        <p className="max-w-xl">
+        <p className="">
           Since 2012, we've been pushing the boundaries of what's possible in web
           design, development, and digital marketing.
         </p>
