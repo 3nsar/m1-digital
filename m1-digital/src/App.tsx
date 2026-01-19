7d547ef304cbfe402c1b5923978abcffd671bfb7
Ensar <ensar_mehmedovic@hotmail.com>
2026-01-19 21:56:34 +0100

    Navbar

--- m1-digital/src/App.tsx
+++ m1-digital/src/App.tsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Details from './components/Details'
 import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 
 
 
@@ -10,6 +11,7 @@ function App() {
 
   return (
     <>
+    <Navbar/>
       <Hero/>
       <About/>
       <Details/>
