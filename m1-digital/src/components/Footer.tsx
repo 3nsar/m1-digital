f6f5c8ef7f0bf8d6d6dfd8005b1578514f8c11d8
Ensar <ensar_mehmedovic@hotmail.com>
2026-01-19 21:55:46 +0100

    Footer update

--- m1-digital/src/components/Footer.tsx
+++ m1-digital/src/components/Footer.tsx
@@ -1,17 +1,18 @@
 export default function Footer() {
     return (
-        <footer className="w-full bg-primary-black text-primary-white">
+        <footer className="w-full bg-primary-black text-primary-white border-t border-primary-white">
             <div className="max-w-7xl mx-auto px-6 py-16 flex flex-col items-center">
                 <div className="flex items-center space-x-3 mb-6">
                     <img alt="" className="h-11"
-                        src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/dummyLogo/prebuiltuiLogoSquareShapeDark.svg" />
+                        src="vite.svg" />
+                        <p className="font-bold text-2xl">M1-DIGITAL</p>
                 </div>
                 <p className="text-center max-w-xl text-sm font-normal leading-relaxed">
                     Empowering creators worldwide with the most advanced AI content creation tools. Transform your ideas
                     into reality.
                 </p>
             </div>
-            <div className="border-t border-slate-200">
+            <div className="border-t border-primary-white">
                 <div className="max-w-7xl mx-auto px-6 py-6 text-center text-sm font-normal">
                     <a href="https://prebuiltui.com">prebuiltui</a> ©2025. All rights reserved.
                 </div>
